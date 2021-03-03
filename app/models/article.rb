57f8014fa7393075b9af36e6dc7444980954d6c9
AymenBida <aymen.bida.00@gmail.com>
2021-03-03 09:27:39 +0100

    Votes: generate votes model and configure associations with users and articles models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :votes
+  has_many :users, through: :votes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :articles
+  has_many :votes
+  has_many :articles, through: :votes
 end
