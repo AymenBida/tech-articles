81497b8f220af5c6a43d48b4eb7a5ad1d4e3fe96
AymenBida <aymen.bida.00@gmail.com>
2021-03-03 09:58:02 +0100

    Categories: generate categories model and configured associations with article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :votes
   has_many :users, through: :votes
+  has_and_belongs_to_many :categories
 end
