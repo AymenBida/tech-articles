6179d1680b4998c0d35cf68fdaf178946a046904
AymenBida <aymen.bida.00@gmail.com>
2021-03-03 16:21:12 +0100

    Login_page: add user registration logic to the controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
-  def login
+  def new
   end
 
-  def logout
+  def create
+  end
+
+  def destroy
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Welcome #{@user.name}! You can now login."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
