33334565e685d95780ea64244ba5b6e5e11c2680
AymenBida <aymen.bida.00@gmail.com>
2021-03-04 16:00:03 +0100

    Articles: create a form to write new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = current_user.articles.build
+    @categories = Category.all.pluck(:name, :id)
+  end
+
+  def create
+    @article = User.first.articles.build(article_params)
+    # @article = current_user.articles.build(author_id: current_user.id, article_params)
+    @article.valid?
+    p @article.errors.full_messages
+    if @article.save
+      puts 'dmldkdmlkzmdlkzmdlk------------------------'
+      @category = @article.categories << Category.find_by(id: article_category_params)
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:author_id, :title, :text, :image)
+  end
+
+  def article_category_params
+    params.require(:articles_categories).permit(:category_id)
+  end
 end
