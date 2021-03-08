ea32366b7e9b5f2795b2218151a636dc3f9091be
AymenBida <aymen.bida.00@gmail.com>
2021-03-08 11:59:39 +0100

    Votes: added mechanics for voting and unvoting articles

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,13 @@
 module VotesHelper
+  def voted?(article)
+    article.votes.where(user_id: current_user.id)
+  end
+
+  def show_vote(article)
+    if voted?(article).empty?
+      link_to fa_icon('heart'), article_votes_path(article.id), method: :post, class: 'article__like'
+    else
+      link_to fa_icon('heart'), article_vote_path(article.id, article.id), method: :delete, class: 'article__dislike'
+    end
+  end
 end
