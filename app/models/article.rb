7e9c431c0805f05f38af6e359fbe5041839a17ae
AymenBida <aymen.bida.00@gmail.com>
2021-03-08 11:58:54 +0100

    Articles: added order_by_most_recent scope

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { in: 2..100 }
   validates :text, presence: true, length: { in: 2..1000 }
 
+  scope :order_by_most_recent, -> { order(created_at: :desc) }
+
   belongs_to :author, class_name: 'User'
   has_many :votes
   has_and_belongs_to_many :categories
