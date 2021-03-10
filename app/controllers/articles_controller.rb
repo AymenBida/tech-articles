53d7bce512a8aabaf99e5ca518ce602ce836c431
AymenBida <aymen.bida.00@gmail.com>
2021-03-10 11:05:18 +0100

    Articles: add logic to edit your article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,39 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    set_article
+    @categories = Category.all.pluck(:name, :id)
+    @category = @article.categories.first
+  end
+
+  def update
+    set_article
+    @category = @article.categories.take
+    @categories = Category.all.pluck(:name, :id)
+    if @article.update(article_params)
+      unless @category.id == article_category_params[:category_id].to_i
+        @category.priority -= 1
+        @category.save
+        @article.categories.destroy_all
+        @new_category = Category.find_by(id: article_category_params[:category_id])
+        @article.categories << @new_category
+        @new_category.priority += 1
+        @new_category.save
+      end
+      redirect_to categories_path, notice: 'You successfully edited your article'
+    else
+      @article.valid?
+      flash.now.alert = "Sorry! #{@article.errors.full_messages.first}!"
+      render :edit
+    end
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :text, :image)
